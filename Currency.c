df2ad7612bff49c63287a2c85af1631cc597c49f
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2020-10-03 22:30:20 +0600

    [MOVED][Currency.c] Functions to Custom Header File (CustomHeader.h)

--- Currency.c
+++ Currency.c
@@ -2,14 +2,11 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <string.h>
+#include "CustomHeader.h"
 #include "rlutil.h"
 // "rlutil.h" Custom header file to use colors efficiently in cross platform
 // REF: https://github.com/tapio/rlutil
 
-void inst();
-double scanjson();
-void fetchJson(char[], char[]);
-void timeName(char[], char[]);
 void saveOut(double, double, char[], char[]);
 
 #define MAX_LINE 128
@@ -22,6 +19,7 @@ typedef struct
 
 {
     char ch;
+    char url[MAX_LINE] = "https://free.currconv.com/api/v7/convert?q=USD_BDT&compact=ultra&apiKey=6cb174e127df4a1139f6";
     saveDefaultColor();
 start:
     inst();
@@ -37,8 +35,16 @@ Input:
 
     strupr(strncpy(currType1, currInput.currType, 3));
     strupr(strcpy(currType2, &currInput.currType[4]));
-    fetchJson(currType1, currType2);
-    rate = scanjson();
+
+    // Replacing '-' with '_' inside inputted currency type to match api url format.
+    replace_char(currInput.currType, '-', '_');
+    // creating api url according to user input
+    replace_url1(url, currInput.currType);
+    // Fetching API
+    fetchJson(url, "currencyRate.json");
+    // Scanning exchange rate
+    rate = scanjson("currencyRate.json");
+
     if (rate == -1)
     {
         setColor(RED);
@@ -54,6 +60,7 @@ Input:
         goto Input;
     }
 
+    // Console Output
     printf(" Current Rate: 1 %s = %.4lf %s\n", currType1, rate, currType2);
     setColor(LIGHTCYAN);
     printf(" Total: %.02lf %s\n", (rate * currInput.amount), currType2);
@@ -61,6 +68,7 @@ Input:
     printf(" Calculation: %.4lf %s x %.4lf %s = %.02lf %s\n", currInput.amount, currType1, rate, currType2, (rate * currInput.amount), currType2);
     resetColor();
     hidecursor();
+
 ConfirmationEnd:
     printf("\n =================================================================================== ");
     printf("\n ||    ");
@@ -109,81 +117,8 @@ ConfirmationEnd:
     return 0;
 }
 
-// Could be use json parser library but that will be overkill for
-// this small amount of work.
-double scanjson()
-{
-    int i = 11, j = 0;
-    char rate[30] = "", temp[30] = "";
-    FILE *fp;
-
-    fp = fopen("currencyRate.json", "r");
-    if (fp == NULL)
-        return -1;
-    // Assigning first line of 'convert.json' into 'temp' array
-    fscanf(fp, "%s", &temp);
-    fclose(fp);
-    remove("currencyRate.json");
-    // checking invalid input
-    if (strcmp(temp, "{}") == 0)
-        return -2;
-
-    // Copying double from 'temp' to 'rate' as a string.
-    // NOTE: First 10 index will always be character type because
-    //       of json format, number type will start from index 11.
-    while (temp[i] != '}')
-    {
-        rate[j] = temp[i];
-        i++;
-        j++;
-    }
-    // Converting 'rate' array into double using 'atof'
-    // function from 'stdlib.h' and returning the value.
-    return atof(rate);
-}
-
-// NOTE: This function will run in only Windows.
-// Requirement: Internet Explorer 3 or higher or just urlmon.dll library.
-//              (This library pre-built is into all Windows OS.)
-
-void fetchJson(char x[], char y[])
 void saveOut(double amount, double rate, char currType1[], char currType2[])
 {
-    // REF: https://www.go4expert.com/articles/download-file-using-urldownloadtofile-c-t28721/
-    int i = 43, j = 0;
-    // This is a free API. It has limit, maximum 100 request per hour.
-    char url[MAX_LINE] = "https://free.currconv.com/api/v7/convert?q=USD_BDT&compact=ultra&apiKey=6cb174e127df4a1139f6";
-    char destination[MAX_LINE] = "currencyRate.json";
-    char buffer[MAX_LINE];
-
-    // Replacing 'USD_BDT' with inputted currency types using 2 while loop.
-    // Because in this case(index are known) this one seems more efficient than
-    // recurrtion or any other pre-built function of C.
-    while (url[i] != '_')
-    {
-        url[i] = x[j];
-        j++;
-        i++;
-    }
-    i++;
-    j = 0;
-    while (url[i] != '&')
-    {
-        url[i] = y[j];
-        j++;
-        i++;
-    }
-
-    HRESULT dl;
-
-    typedef HRESULT(WINAPI * URLDownloadToFileA_t)(LPUNKNOWN pCaller, LPCSTR szURL, LPCSTR szFileName, DWORD dwReserved, void *lpfnCB);
-    URLDownloadToFileA_t xURLDownloadToFileA;
-    xURLDownloadToFileA = (URLDownloadToFileA_t)GetProcAddress(LoadLibraryA("urlmon"), "URLDownloadToFileA");
-
-    dl = xURLDownloadToFileA(NULL, url, destination, 0, NULL);
-
-    // Print only error messege if occurs.
-    if (dl == S_OK)
     FILE *fptr;
     char buff[100] = "";
     char x[100] = "";
@@ -197,22 +132,6 @@ void saveOut(double amount, double rate, char currType1[], char currType2[])
     {
         printf("Error!");
         exit(1);
-    }
-    else if (dl == E_OUTOFMEMORY)
-    {
-        setColor(RED);
-        sprintf(buffer, "\n ERROR: Failed To Fetch API! \nREASON: Insufficient Memory!\n");
-        printf(buffer);
-        resetColor();
-        exit(0);
-    }
-    else
-    {
-        setColor(RED);
-        sprintf(buffer, "\n ERROR: Failed To Fetch API! \nREASON: Internet Connction not Available!\n");
-        printf(buffer);
-        resetColor();
-        exit(0);
     }
     fprintf(fptr, "Current Rate: 1 %s = %.4lf %s\n", currType1, rate, currType2);
     fprintf(fptr, "Total: %.02lf %s\n", (rate * amount), currType2);
@@ -242,10 +161,5 @@ void inst()
     resetColor();
 }
 
-void timeName(char data[], char prefix[])
 {
-    time_t now = time(0);
-    // Storing Current Time in 'data'
-    strftime(data, 100, "_%Y-%m-%d_%H-%M-%S.txt", localtime(&now));
-    strcat(prefix, data);
 }
---
+++ CustomHeader.h
@@ -0,0 +1,133 @@
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+void timeName(char data[], char prefix[])
+{
+    time_t now = time(0);
+    // Storing Current Time in 'data'
+    strftime(data, 100, "_%Y-%m-%d_%H-%M-%S.txt", localtime(&now));
+    //
+    strcat(prefix, data);
+}
+
+void replace_char(char *str, char oldChar, char newChar)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == oldChar)
+        {
+            str[i] = newChar;
+        }
+        i++;
+    }
+}
+
+void replace_url1(char url[], char x[])
+{
+    int i = 43, j = 0;
+    while (url[i] != '&')
+    {
+        url[i] = x[j];
+        j++;
+        i++;
+    }
+}
+
+void replace_url2(char url[], char x[], char y[])
+{
+    int i = 43, j = 0;
+    while (url[i] != '_')
+    {
+        url[i] = x[j];
+        j++;
+        i++;
+    }
+    i++;
+    j = 0;
+    while (url[i] != '&')
+    {
+        url[i] = y[j];
+        j++;
+        i++;
+    }
+}
+
+// Could be use json parser library but that will be overkill for
+// this small amount of work.
+double scanjson(char jsonName[])
+{
+    int i = 11, j = 0;
+    char rate[30] = "", temp[30] = "";
+    FILE *fp;
+
+    fp = fopen(jsonName, "r");
+    if (fp == NULL)
+        return -1;
+    // Assigning first line of 'convert.json' into 'temp' array
+    fscanf(fp, "%s", &temp);
+    fclose(fp);
+    remove(jsonName);
+    // checking invalid input
+    if (strcmp(temp, "{}") == 0)
+        return -2;
+
+    // Copying double from 'temp' to 'rate' as a string.
+    // NOTE: First 10 index will always be character type because
+    //       of json format, number type will start from index 11.
+    while (temp[i] != '}')
+    {
+        rate[j] = temp[i];
+        i++;
+        j++;
+    }
+    // Converting 'rate' array into double using 'atof'
+    // function from 'stdlib.h' and returning the value.
+    return atof(rate);
+}
+
+// NOTE: This function will run in only Windows.
+// Requirement: Internet Explorer 3 or higher or just urlmon.dll library.
+//              (This library pre-built is into all Windows OS.)
+
+void fetchJson(char url[], char destination[])
+{
+    // REF: https://www.go4expert.com/articles/download-file-using-urldownloadtofile-c-t28721/
+    int i = 43, j = 0;
+    // This is a free API. It has limit, maximum 100 request per hour.
+    char buffer[256];
+
+    // Replacing 'USD_BDT' with inputted currency types using 2 while loop.
+    // Because in this case(index are known) this one seems more efficient than
+    // recurrtion or any other pre-built function of C.
+
+    HRESULT dl;
+
+    typedef HRESULT(WINAPI * URLDownloadToFileA_t)(LPUNKNOWN pCaller, LPCSTR szURL, LPCSTR szFileName, DWORD dwReserved, void *lpfnCB);
+    URLDownloadToFileA_t xURLDownloadToFileA;
+    xURLDownloadToFileA = (URLDownloadToFileA_t)GetProcAddress(LoadLibraryA("urlmon"), "URLDownloadToFileA");
+
+    dl = xURLDownloadToFileA(NULL, url, destination, 0, NULL);
+
+    // Print only error messege if occurs.
+    if (dl == S_OK)
+    {
+    }
+    else if (dl == E_OUTOFMEMORY)
+    {
+        setColor(RED);
+        sprintf(buffer, "\n ERROR: Failed To Fetch API! \nREASON: Insufficient Memory!\n");
+        printf(buffer);
+        resetColor();
+        exit(0);
+    }
+    else
+    {
+        setColor(RED);
+        sprintf(buffer, "\n ERROR: Failed To Fetch API! \nREASON: Internet Connction not Available!\n");
+        printf(buffer);
+        resetColor();
+        exit(0);
+    }
+}
