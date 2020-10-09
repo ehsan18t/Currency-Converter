01445416d766b548526631cbf3f68a2874923a0e
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2020-10-09 08:47:15 +0600

    [ADDED][MyHistory.c] 7 Days History & 7 Days Best from History

--- bin/MyHistory.c
+++ bin/MyHistory.c
@@ -4,13 +4,26 @@
 #include <time.h>
 #include "headers/rlutil.h"
 
+typedef struct
+{
+    char date[12];
+} myDate;
+
+// Global variable declare
+myDate sevenDate[7] = {""};
+
 void myHistoryMainMenu();
 void myHistoryTop();
 void currencyConversionHistory();
 void autoRateCheckingHistory();
+void currencyConversionHistory7D();
+void autoRateCheckingHistory7D();
 void bestConversionPrice();
+void bestConversionPrice7D();
 void bestAutoRate();
+void bestAutoRate7D();
 void myHistoryEndOpt();
+void myHistoryEndOpt2();
 
 //
 // Main Function
@@ -18,6 +31,9 @@ void myHistoryEndOpt();
 int myHistory()
 {
     char ch, ch2;
+    // Adding Last 7 Dates to the array
+    for (int i = 0; i < 7; i++)
+        previousDate(sevenDate[i].date, i);
     saveDefaultColor();
 myHistoryMainMenu:
     cls();
@@ -33,7 +49,7 @@ myHistoryMainMenu:
         cls();
         hidecursor();
         currencyConversionHistory();
-        myHistoryEndOpt();
+        myHistoryEndOpt2();
     opt1:
         ch2 = getch();
         tolower(ch2);
@@ -41,6 +57,13 @@ myHistoryMainMenu:
         {
             showcursor();
             goto myHistoryMainMenu;
+        }
+        else if (ch2 == 'h')
+        {
+            cls();
+            bestConversionPrice();
+            myHistoryEndOpt();
+            goto opt1;
         }
         else if (ch2 == 'm')
         {
@@ -63,7 +86,7 @@ myHistoryMainMenu:
         cls();
         hidecursor();
         autoRateCheckingHistory();
-        myHistoryEndOpt();
+        myHistoryEndOpt2();
     opt2:
         ch2 = getch();
         tolower(ch2);
@@ -71,6 +94,13 @@ myHistoryMainMenu:
         {
             showcursor();
             goto myHistoryMainMenu;
+        }
+        else if (ch2 == 'h')
+        {
+            cls();
+            bestAutoRate();
+            myHistoryEndOpt();
+            goto opt2;
         }
         else if (ch2 == 'm')
         {
@@ -92,8 +122,8 @@ myHistoryMainMenu:
     {
         cls();
         hidecursor();
-        bestConversionPrice();
-        myHistoryEndOpt();
+        currencyConversionHistory7D();
+        myHistoryEndOpt2();
     opt3:
         ch2 = getch();
         tolower(ch2);
@@ -101,6 +131,13 @@ myHistoryMainMenu:
         {
             showcursor();
             goto myHistoryMainMenu;
+        }
+        else if (ch2 == 'h')
+        {
+            cls();
+            bestConversionPrice7D();
+            myHistoryEndOpt();
+            goto opt3;
         }
         else if (ch2 == 'm')
         {
@@ -122,8 +159,8 @@ myHistoryMainMenu:
     {
         cls();
         hidecursor();
-        bestAutoRate();
-        myHistoryEndOpt();
+        autoRateCheckingHistory7D();
+        myHistoryEndOpt2();
     opt4:
         ch2 = getch();
         tolower(ch2);
@@ -131,6 +168,13 @@ myHistoryMainMenu:
         {
             showcursor();
             goto myHistoryMainMenu;
+        }
+        else if (ch2 == 'h')
+        {
+            cls();
+            bestAutoRate7D();
+            myHistoryEndOpt();
+            goto opt4;
         }
         else if (ch2 == 'm')
         {
@@ -145,7 +189,7 @@ myHistoryMainMenu:
         else
         {
             wrongInput();
-            goto opt3;
+            goto opt4;
         }
     }
     else if (ch == 'm')
@@ -224,6 +268,74 @@ void autoRateCheckingHistory()
     fclose(currConvHistory);
 }
 
+void currencyConversionHistory7D()
+{
+    setColor(LIGHTRED);
+    printf("\n                ==============================================        \n");
+    printf("                ||   Currency Conversion History (7 DAYS)   ||        \n");
+    printf("                ==============================================       \n");
+    resetColor();
+    FILE *currConvHistory;
+    history data;
+    char currConveHistoryLocation[128] = "History/CurrencyCalculationHistory.txt";
+    currConvHistory = fopen(currConveHistoryLocation, "r");
+    if (currConvHistory == NULL)
+    {
+        printf("Error!\n");
+        exit(0);
+    }
+
+    printf("\n");
+    printf("    Date         Time       Amount      Exchange ID     Exchange Rate      Total\n");
+    printf("   ------       ------    ----------   -------------   ---------------    --------\n");
+    while (!feof(currConvHistory))
+    {
+        fscanf(currConvHistory, "%s %s %lf %s %lf %lf", &data.date, &data.time, &data.amount, &data.type, &data.rate, &data.total);
+        for (int i = 0; i < 7; i++)
+        {
+            if (strcmp(sevenDate[i].date, data.date) == 0)
+            {
+                printf(" %s    %s     %.4lf        %s          %.4lf        %.4lf\n", data.date, data.time, data.amount, data.type, data.rate, data.total);
+            }
+        }
+    }
+    fclose(currConvHistory);
+}
+
+void autoRateCheckingHistory7D()
+{
+    setColor(LIGHTRED);
+    printf("\n    ==================================================        \n");
+    printf("    ||   Automatic Rate Checking History (7 DAYS)   ||        \n");
+    printf("    ==================================================       \n");
+    resetColor();
+    FILE *currConvHistory;
+    history data;
+    char currConveHistoryLocation[128] = "History/AutoCheckerHistory.txt";
+    currConvHistory = fopen(currConveHistoryLocation, "r");
+    if (currConvHistory == NULL)
+    {
+        printf("Error!\n");
+        exit(0);
+    }
+
+    printf("\n");
+    printf("    Date         Time      Exchange ID     Exchange Rate\n");
+    printf("   ------       ------    -------------   ---------------\n");
+    while (!feof(currConvHistory))
+    {
+        fscanf(currConvHistory, "%s %s %s %lf", &data.date, &data.time, &data.type, &data.rate);
+        for (int i = 0; i < 7; i++)
+        {
+            if (strcmp(sevenDate[i].date, data.date) == 0)
+            {
+                printf(" %s    %s      %s          %.4lf\n", data.date, data.time, data.type, data.rate);
+            }
+        }
+    }
+    fclose(currConvHistory);
+}
+
 void bestConversionPrice()
 {
     setColor(LIGHTRED);
@@ -234,7 +346,7 @@ void bestConversionPrice()
     int i = 0, flag = -1;
     FILE *currConvHistory;
     history data;
-    // 20 different types exchange's highest price can be displayed
+    // 100 different types exchange's highest price can be displayed
     history highest[100] = {"", "", "", 0, 0, 0};
     char currConveHistoryLocation[128] = "History/CurrencyCalculationHistory.txt";
     currConvHistory = fopen(currConveHistoryLocation, "r");
@@ -286,17 +398,92 @@ void bestConversionPrice()
     }
 }
 
+void bestConversionPrice7D()
+{
+    setColor(LIGHTRED);
+    printf("\n             =====================================================        \n");
+    printf("             ||   Best Price From Conversion History (7 DAYS)   ||        \n");
+    printf("             =====================================================       \n");
+    resetColor();
+    int i = 0, flag = -1, flag2;
+    FILE *currConvHistory;
+    history data;
+    // 100 different types exchange's highest price can be displayed
+    history highest[100] = {"", "", "", 0, 0, 0};
+    char currConveHistoryLocation[128] = "History/CurrencyCalculationHistory.txt";
+    currConvHistory = fopen(currConveHistoryLocation, "r");
+    if (currConvHistory == NULL)
+    {
+        printf("Error!\n");
+        exit(0);
+    }
+
+    printf("\n");
+    printf("    Date         Time       Amount      Exchange ID     Exchange Rate      Total\n");
+    printf("   ------       ------    ----------   -------------   ---------------    --------\n");
+    while (!feof(currConvHistory))
+    {
+        flag2 = 0;
+        fscanf(currConvHistory, "%s %s %lf %s %lf %lf", &data.date, &data.time, &data.amount, &data.type, &data.rate, &data.total);
+        for (int k = 0; k < 7; k++)
+        {
+            if (strcmp(sevenDate[k].date, data.date) == 0)
+            {
+                flag2 = 1;
+                break;
+            }
+        }
+
+        if (flag2 == 1)
+        {
+            if (strcmp(data.type, highest[i].type) == 0)
+            {
+                if (data.rate > highest[i].rate)
+                {
+                    highest[i] = data;
+                    i++;
+                }
+            }
+            else
+            {
+                flag = 0;
+                for (int j = 0; j <= i; j++)
+                {
+                    if (strcmp(data.type, highest[j].type) == 0)
+                    {
+                        if (data.rate > highest[j].rate)
+                            highest[j] = data;
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    flag = -1;
+                    highest[i] = data;
+                    i++;
+                }
+            }
+        }
+    }
+    fclose(currConvHistory);
+    for (int j = 0; j < i; j++)
+    {
+        printf(" %s    %s     %.4lf        %s          %.4lf        %.4lf\n", highest[j].date, highest[j].time, highest[j].amount, highest[j].type, highest[j].rate, highest[j].total);
+    }
+}
+
 void bestAutoRate()
 {
     setColor(LIGHTRED);
-    printf("\n     ================================================        \n");
-    printf("     ||   Best Auto Rate Checking History Prices   ||        \n");
-    printf("     ================================================       \n");
+    printf("\n     ====================================================        \n");
+    printf("     ||   Best Price From Auto Rate Checking History   ||        \n");
+    printf("     ====================================================       \n");
     resetColor();
     int i = 0, flag = -1;
     FILE *currConvHistory;
     history data;
-    // 20 different types exchange's highest price can be displayed
+    // 100 different types exchange's highest price can be displayed
     history highest[100] = {"", "", "", 0, 0, 0};
     char currConveHistoryLocation[128] = "History/AutoCheckerHistory.txt";
     currConvHistory = fopen(currConveHistoryLocation, "r");
@@ -348,6 +535,81 @@ void bestAutoRate()
     }
 }
 
+void bestAutoRate7D()
+{
+    setColor(LIGHTRED);
+    printf("\n =============================================================        \n");
+    printf(" ||   Best Price From Auto Rate Checking History (7 DAYS)   ||        \n");
+    printf(" =============================================================       \n");
+    resetColor();
+    int i = 0, flag = -1, flag2;
+    FILE *currConvHistory;
+    history data;
+    // 100 different types exchange's highest price can be displayed
+    history highest[100] = {"", "", "", 0, 0, 0};
+    char currConveHistoryLocation[128] = "History/AutoCheckerHistory.txt";
+    currConvHistory = fopen(currConveHistoryLocation, "r");
+    if (currConvHistory == NULL)
+    {
+        printf("Error!\n");
+        exit(0);
+    }
+
+    printf("\n");
+    printf("    Date         Time      Exchange ID     Exchange Rate\n");
+    printf("   ------       ------    -------------   ---------------\n");
+    while (!feof(currConvHistory))
+    {
+        flag2 = 0;
+        fscanf(currConvHistory, "%s %s %s %lf", &data.date, &data.time, &data.type, &data.rate);
+        for (int k = 0; k < 7; k++)
+        {
+            if (strcmp(sevenDate[k].date, data.date) == 0)
+            {
+                flag2 = 1;
+                break;
+            }
+        }
+
+        if (flag2 == 1)
+        {
+            if (strcmp(data.type, highest[i].type) == 0)
+            {
+                if (data.rate > highest[i].rate)
+                {
+                    highest[i] = data;
+                    i++;
+                }
+            }
+            else
+            {
+                flag = 0;
+                for (int j = 0; j <= i; j++)
+                {
+                    if (strcmp(data.type, highest[j].type) == 0)
+                    {
+                        if (data.rate > highest[j].rate)
+                            highest[j] = data;
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    flag = -1;
+                    highest[i] = data;
+                    i++;
+                }
+            }
+        }
+    }
+    fclose(currConvHistory);
+    for (int j = 0; j < i; j++)
+    {
+        printf(" %s    %s      %s          %.4lf\n", highest[j].date, highest[j].time, highest[j].type, highest[j].rate);
+    }
+}
+
 //
 // Headers UI
 //
@@ -385,6 +647,30 @@ void myHistoryEndOpt()
     printf("||\n ====================================================\n");
 }
 
+void myHistoryEndOpt2()
+{
+    printf("\n ===================================================================");
+    printf("\n ||   ");
+    setColor(LIGHTMAGENTA);
+    printf("[B]");
+    setColor(YELLOW);
+    printf(" Back    ");
+    setColor(LIGHTMAGENTA);
+    printf("[H]");
+    setColor(YELLOW);
+    printf(" Highest Rate    ");
+    setColor(LIGHTMAGENTA);
+    printf("[M]");
+    setColor(YELLOW);
+    printf(" Main Menu     ");
+    setColor(LIGHTMAGENTA);
+    printf("[X]");
+    setColor(YELLOW);
+    printf(" Exit  ");
+    resetColor();
+    printf("||\n ===================================================================\n");
+}
+
 //
 // Main Menu UI
 //
@@ -396,24 +682,24 @@ void myHistoryMainMenu()
     setColor(LIGHTCYAN);
     printf(" |");
     resetColor();
-    printf("    1. Currency Conversion History");
-    setColor(LIGHTCYAN);
-    printf("                    |\n");
-    printf(" |");
-    resetColor();
-    printf("    2. Automatic Rate Checking History");
-    setColor(LIGHTCYAN);
-    printf("                |\n");
-    printf(" |");
-    resetColor();
-    printf("    3. Best Price from Conversion History");
+    printf("    1. Currency Conversion History (FULL)");
     setColor(LIGHTCYAN);
     printf("             |\n");
     printf(" |");
     resetColor();
-    printf("    4. Best Prices from Auto Rate Checking History");
+    printf("    2. Automatic Rate Checking History (FULL)");
     setColor(LIGHTCYAN);
-    printf("    |\n");
+    printf("         |\n");
+    printf(" |");
+    resetColor();
+    printf("    3. Currency Conversion History (7 DAYS)");
+    setColor(LIGHTCYAN);
+    printf("           |\n");
+    printf(" |");
+    resetColor();
+    printf("    4. Automatic Rate Checking History (7 DAYS)");
+    setColor(LIGHTCYAN);
+    printf("       |\n");
     printf(" |");
     resetColor();
     printf("    M. Main Menu");
